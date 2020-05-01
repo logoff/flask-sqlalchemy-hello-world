5437472aef343df9c6ae3238b0b06d35720046fb
logoff <juliocarlos@gmail.com>
2020-05-01 16:55:14 +0200

    Print Python version in /hello.

--- fshw/fshw.py
+++ fshw/fshw.py
@@ -1,5 +1,7 @@
-from flask import Flask
+import sys
+from typing import List
 
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -7,4 +9,5 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/hello')
 def hello():
-    return 'Hello!'
+
+    return f'Hello!\nPython {sys.version}\n'
